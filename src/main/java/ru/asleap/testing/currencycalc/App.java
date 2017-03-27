9d5c666832b1c34cee4f322b0420cd449bc6b558
Timofey Lyssenko <tim.lyssenko@gmail.com>
2017-03-27 21:16:40 +0300

    Initial commit with several dependencies set in pom.xml

---
+++ src/main/java/ru/asleap/testing/currencycalc/App.java
@@ -0,0 +1,10 @@
+package ru.asleap.testing.currencycalc;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
